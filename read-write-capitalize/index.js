5428c6d76c9061aafd914609e3a8d8f389a094f7
Stephen <smjarr89@gmail.com>
2018-06-13 15:15:46 -0400

    added the read-write program exercise

---
+++ read-write-capitalize/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const readline = require('readline-sync');
+
+inputFile = readline.question("What file do you want to read and capitalize? Please include directory in your answer as well as the file extension: ");
+outputFile = readline.question("What file do you want to read and capitalize? Please include directory in your answer as well as the file extension: ");
+
+function capitalization(content) {
+    return content.toUpperCase();
+}
+
+function writeAFile(dataCapitalized) {
+    fs.writeFile(outputFile, dataCapitalized, err => {
+        if (err) {
+            return console.log(err.message);
+        }})
+    console.log(`The file was saved as ${outputFile}!`);
+}
+
+fs.readFile(inputFile, function (err, buffer) {
+    if (err) {
+        console.log(err.message);
+    }
+    var data = buffer.toString();
+    var dataCapitalized = capitalization(data);
+    writeAFile(dataCapitalized);
+})
